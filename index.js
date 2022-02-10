699e60b9bd787577827b4f6645a9419c87defc05
Cheisyya <cheisya_valda_29rpl@student.smktelkom.mlg.sch.id>
2022-02-10 08:34:46 +0700

    toko komputer

---
+++ index.js
@@ -0,0 +1,25 @@
+//Import 
+const express = require("express")
+const cors = require("cors")
+
+//Implementasi
+const app = express()
+app.use(cors())
+
+//Endpoint Admin
+const admin = require('./routes/admin')
+app.use("/admin", admin)
+
+//Endpoint customer
+const customer = require('./routes/customer');
+app.use("/customer", customer)
+
+//Endpoint product
+const product = require('./routes/product');
+app.use("/product", product)
+
+//Run Server (Penutup)
+app.listen(1010, () => {
+    console.log("Server run on port 1010");
+})
+
